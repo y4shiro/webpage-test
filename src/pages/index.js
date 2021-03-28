20db2e5d052ea05e5397fa7f12efa08aea63d883
y4shiro <y4shironao@gmail.com>
2021-03-28 21:32:26 +0900

    Header 追加

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,18 @@
 import React from "react"
 
+import { ChakraProvider, Flex, Heading } from "@chakra-ui/react"
+
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <ChakraProvider>
+      <Flex as="header" padding="1.5rem" bg="teal.500" color="white">
+        <Flex align="center" mr={5}>
+          <Heading as="h1" size="md">
+            Header
+          </Heading>
+        </Flex>
+      </Flex>
+      <div>Content</div>
+    </ChakraProvider>
+  )
 }
